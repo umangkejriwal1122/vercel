0430267da31392fe9de7b0cba304bd4ba48bf04e
umang <umang20072007@gmail.com>
2023-02-11 18:03:30 +0530

    x

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "Build Start"
-apt install sudo
-sudo apt install libpq-dev python3-dev
+python3.9 -m pip install psycopg2-binary
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build Complete"
